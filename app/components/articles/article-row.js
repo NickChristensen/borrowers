d5b1a28b024b6651c4b49e37c8c1fd38d9ba0c39
Nick Christensen <nickc@spiceworks.com>
2015-07-27 13:16:32 -0500

    update component save action

--- app/components/articles/article-row.js
+++ app/components/articles/article-row.js
@@ -4,16 +4,12 @@ export default Ember.Component.extend({
 	tagName: 'tr',
 	article: null, //passed in
 	articleStates: null, //passed in
-	autoSave: function(){
-		var article = this.get('article');
-		if ( !article.get('isNew') ){
-			this.sendAction('save', article);
+	actions: {
+		saveArticle() {
+			let article = this.get('article');
+			if( article.get('hasDirtyAttributes') ) {
+				this.sendAction('save', article);
+			}
 		}
-	},
-	stateChanged: Ember.on('init', Ember.observer('article.state', function(){
-		var article = this.get('article');
-		if( article.get('isDirty') && !article.get('isSaving') ){
-			Ember.run.once(this, this.autoSave);
-		}
-	}))
+	}
 });
