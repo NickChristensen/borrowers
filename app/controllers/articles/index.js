a2857a4dc3295c8c65d2ee85af344973904ce482
Nick Christensen <nick.g.christensen@gmail.com>
2015-12-17 23:11:49 -0600

    Add default "Borrowed" filter to articles

--- app/controllers/articles/index.js
+++ app/controllers/articles/index.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	queryParams: ['show'],
-	possibleStates: ['borrowed', 'returned']
+	queryParams: ['showReturned'],
+	possibleStates: ['borrowed', 'returned'],
+  
+  showReturned: false,
+  
+  filteredResults: Ember.computed('showReturned', 'model', function() {
+    if( this.get('showReturned') ) {
+      return this.get('model');
+    } else {
+      return this.get('model').filterBy('state', 'borrowed');
+    }
+  })
 });
